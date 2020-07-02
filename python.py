8848b08ed3537027ab62bf033ae3c513a9aab065
KananAzim <66919303+KananAzim@users.noreply.github.com>
2020-07-02 21:57:06 +0400

    Create python.py

---
+++ python.py
@@ -0,0 +1,13 @@
+
+
+while True:
+        
+    p = ["1.Print all workers","2.Add worker","3.Delete worker","4.Exit"]
+    for n in p:
+        print(n, end= "\n")
+    print()
+
+    k = int(input("Enter:"))
+    if k==4:
+         break
+
